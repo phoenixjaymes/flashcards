803575d85d64c0f47c22d9251d881365f6f330ef
Jaymes Young <jaymes@phoenixjaymes.com>
2016-09-28 15:50:36 -0700

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,12 @@
+/* 
+ File     : main.js
+ Date     : Sep 28, 2016
+ Author   : Jaymes Young <jaymes@phoenixjaymes.com>
+ */
+
+'use strict';
+
+angular.module('flashcards')
+    .controller('Main', function($scope) {
+      
+});
