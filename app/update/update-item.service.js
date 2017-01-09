86ad1c724b7c71249f533450d842f83aaf04f477
Jaymes Young <jaymes@phoenixjaymes.com>
2017-01-09 09:30:29 -0800

    added updateLastPracticed function

--- app/update/update-item.service.js
+++ app/update/update-item.service.js
@@ -9,19 +9,27 @@
 angular.module('flashcards')
   .service('updateItemService', function($http, $httpParamSerializerJQLike) {
     
-    // Get word
+    // Find word to update
     this.findItem = function(item, callback) {
       var url = 'assets/inc/fc-get-update-item.php';
       var config = {params: item};
       $http.get(url, config).then(callback);
     };
 
-    // Add items
+    // Update item
     this.updateItem = function(item, callback) {
       var url = 'assets/inc/fc-update-item.php';
       var config = {headers: {'Content-Type': 'application/x-www-form-urlencoded'}};
 
       $http.post(url, $httpParamSerializerJQLike(item), config).then(callback); 
     };
-      
+    
+    
+    // Update last practiced
+    this.updateLastPracticed = function(item, callback) {
+      var url = 'assets/inc/fc-update-last-practiced.php';
+      var config = {headers: {'Content-Type': 'application/x-www-form-urlencoded'}};
+
+      $http.post(url, $httpParamSerializerJQLike(item), config).then(callback); 
+    };
 });
