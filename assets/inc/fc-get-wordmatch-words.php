29b7eba9987fe92904555d0ca5d4e75886ba7d6f
Jaymes Young <jaymes@phoenixjaymes.com>
2017-01-09 09:12:15 -0800

    New files for word match

---
+++ assets/inc/fc-get-wordmatch-words.php
@@ -0,0 +1,155 @@
+<?php
+/* 
+ * File Name: fc-get-update-words.php
+ * Date: 21 Dec 16
+ * Programmer: Jaymes Young-Liebgott
+ */
+
+require 'db-constants.php';
+require '../classes/database.php';
+require 'fc-utilities.php';
+
+// Set content ype to json
+header('content-type: application/json; charset=utf-8');
+
+// Databse connection
+$mySqli = new Database(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+$mySqli->getConnection();
+  
+$arr_cards = [];
+
+
+$pos = filter_input(INPUT_GET, 'pos', FILTER_SANITIZE_STRING);
+
+if ($pos === 'phrase') {
+  // Phares
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  
+  $sql = "SELECT id, english, translation, 'none' AS gender FROM fc_german_phrases";
+  $result = $mySqli->handleQuery($sql);
+  
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_card = [];
+    $arr_card['id'] = $row['id'];
+    $arr_card['english'] = $row['english'];
+    $arr_card['translation'] = $row['translation'];
+    $arr_card['gender'] = $row['gender'];
+
+    $arr_cards[] = $arr_card;
+  }
+
+  send_data($arr_cards);  
+  
+} elseif ($pos === 'verb') {
+  // Verbs
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  $sort = filter_input(INPUT_GET, 'sort', FILTER_SANITIZE_STRING);
+  
+  
+  $sql = "SELECT id, english, translation, ich, du, er_sie_es, wir, ihr, sie_Sie, img, 'none' AS gender FROM fc_german_verbs ORDER BY translation";
+  
+  $result = $mySqli->handleQuery($sql);
+  
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_card = [];
+    $arr_card['id'] = $row['id'];
+    $arr_card['english'] = $row['english'];
+    $arr_card['translation'] = $row['translation'];
+    $arr_card['ich'] = $row['ich'];
+    $arr_card['du'] = $row['du'];
+    $arr_card['er_sie_es'] = $row['er_sie_es'];
+    $arr_card['wir'] = $row['wir'];
+    $arr_card['ihr'] = $row['ihr'];
+    $arr_card['sie_Sie'] = $row['sie_Sie'];
+    $arr_card['img'] = $row['img'];
+    $arr_card['gender'] = $row['gender'];
+
+    $arr_cards[] = $arr_card;
+  }
+
+  send_data($arr_cards);   
+  
+} elseif ($pos === 'adjective') {
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  
+  // Adjectives
+  $sql = "SELECT id, english, translation, img, 'none' AS gender FROM fc_german_adjectives WHERE category = '{$category}' ORDER BY translation";
+  
+  
+  $result = $mySqli->handleQuery($sql);
+
+
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_card = [];
+    $arr_card['id'] = $row['id'];
+    $arr_card['english'] = $row['english'];
+    $arr_card['translation'] = $row['translation'];
+    $arr_card['img'] = $row['img'];
+    $arr_card['gender'] = $row['gender'];
+
+    $arr_cards[] = $arr_card;
+  }
+
+  send_data($arr_cards); 
+  
+} elseif ($pos === 'noun') {
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  
+  // Nouns
+  $sql = "SELECT fc_german_nouns.id, english, translation, img, fc_categories_gender.gender AS gender"
+    . " FROM fc_german_nouns, fc_categories_gender"
+    . " WHERE fc_german_nouns.gender = fc_categories_gender.id AND category = '{$category}'"
+    . " ORDER BY translation";
+  
+  $result = $mySqli->handleQuery($sql);
+
+
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_card = [];
+    $arr_card['id'] = $row['id'];
+    $arr_card['english'] = $row['english'];
+    $arr_card['translation'] = $row['translation'];
+    $arr_card['img'] = $row['img'];
+    $arr_card['gender'] = $row['gender'];
+
+    $arr_cards[] = $arr_card;
+  }
+
+  send_data($arr_cards); 
+  
+} else {
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  
+  // Nouns and adjectives
+  if (isset($_GET) && isset($_GET['category']) && $_GET['category'] === 'all') {
+    $sql = "(SELECT english, translation, img, 'none' AS gender FROM fc_german_adjectives LIMIT 15)"
+        . " UNION "
+        . " (SELECT english, translation, img, gender FROM fc_german_nouns LIMIT 15)";
+  } else if (isset($_GET) && isset($_GET['category'])) {
+    $sql = "SELECT english, translation, img, 'none' AS gender FROM fc_german_adjectives WHERE category = {$category}"
+        . " UNION "
+        . " SELECT english, translation, img, gender FROM fc_german_nouns      WHERE category = {$category}";
+  } else {
+     $sql = "SELECT * FROM fc_german_nouns WHERE category = 5 LIMIT 20";
+   }
+
+
+  $result = $mySqli->handleQuery($sql);
+
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_card = [];
+    $arr_card['english'] = $row['english'];
+    $arr_card['translation'] = $row['translation'];
+    $arr_card['img'] = $row['img'];
+    $arr_card['gender'] = $row['gender'];
+
+    $arr_cards[] = $arr_card;
+  }
+
+  send_data($arr_cards);
+}
---
+++ assets/inc/fc-update-last-practiced.php
@@ -0,0 +1,231 @@
+<?php
+/* 
+ * File Name: fc-update-item.php
+ * Date: 22 Dec 16
+ * Programmer: Jaymes Young-Liebgott
+ */
+
+require 'db-constants.php';
+require '../classes/database.php';
+require 'fc-utilities.php';
+
+// Set content ype to json
+header('content-type: application/json; charset=utf-8');
+
+// Databse connection
+$mySqli = new Database(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+  $isConnected = $mySqli->getConnection();
+
+// Set linkId if connected
+ $isConnected ? $linkId = $mySqli->getLink() : $linkId = false;
+ 
+ /*
+  * mysqli->real escape string should be added to class
+  */
+ 
+ 
+$arr_response = [];
+$date = date('Y-m-d');
+
+$pos = filter_input(INPUT_POST, 'pos', FILTER_SANITIZE_STRING);
+
+//print_r($_POST);
+//exit();
+
+
+//
+if ($pos && $pos === 'adjective') {
+  // Sanitize input
+  $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+  $english = filter_input(INPUT_POST, 'english', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $translation = filter_input(INPUT_POST, 'translation', FILTER_SANITIZE_STRING);
+  $img = filter_input(INPUT_POST, 'image', FILTER_SANITIZE_STRING);
+  $category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
+  
+  // Escape input
+  $id_safe = $linkId->real_escape_string($id);
+  $english_safe = $linkId->real_escape_string($english);
+  $translation_safe = $linkId->real_escape_string($translation);
+  $img_safe = $linkId->real_escape_string($img);
+  $category_safe = $linkId->real_escape_string($category);
+   
+  // Check of empty values
+  if (!$id_safe || !$english_safe || !$translation_safe || !$img_safe || !$category_safe) {
+    $arr_response['success'] = 'incorrect';
+  } else {
+         
+    $sql = "UPDATE fc_german_adjectives"
+      . " SET english = '$english_safe', "
+      . " translation = '$translation_safe', "
+      . " img = '$img_safe', "
+      . " category = '$category_safe' "
+      . " WHERE id = $id_safe";
+
+    $result = $mySqli->handleQuery($sql);
+
+    if ($result) {
+      $arr_response['success'] = 'updated';
+    } else {
+      $arr_response['success'] = false;
+    }
+  }
+  
+  send_data($arr_response);
+  
+} elseif ($pos && $pos === 'noun') {
+  // Sanitize input
+  $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+  $english = filter_input(INPUT_POST, 'english', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $translation = filter_input(INPUT_POST, 'translation', FILTER_SANITIZE_STRING);
+  $img = filter_input(INPUT_POST, 'image', FILTER_SANITIZE_STRING);
+  $gender = filter_input(INPUT_POST, 'gender', FILTER_SANITIZE_STRING);
+  $category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
+  
+  // Escape input
+  $id_safe = $linkId->real_escape_string($id);
+  $english_safe = $linkId->real_escape_string($english);
+  $translation_safe = $linkId->real_escape_string($translation);
+  $img_safe = $linkId->real_escape_string($img);
+  $category_safe = $linkId->real_escape_string($category);
+  
+  if (!$id_safe || !$english_safe || !$translation_safe || !$img_safe || !$gender || !$category_safe) {
+    $arr_response['success'] = 'incorrect';
+    send_data($arr_response);
+  } else {
+
+    $sql = "UPDATE fc_german_nouns"
+      . " SET english = '$english_safe', "
+      . " translation = '$translation_safe', "
+      . " img = '$img_safe', "
+      . " gender = $gender, "
+      . " category = '$category_safe' "
+      . " WHERE id = $id_safe";
+
+    $result = $mySqli->handleQuery($sql);
+
+    if ($result) {
+      $arr_response['success'] = 'updated';
+      
+    } else {
+      $arr_response['success'] = false;
+    }
+  }
+  
+  send_data($arr_response);
+    
+} elseif ($pos && $pos === 'phrase') {
+  // Sainitize input
+  $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+  $english = filter_input(INPUT_POST, 'english', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $translation = filter_input(INPUT_POST, 'translation', FILTER_SANITIZE_STRING);
+  
+  // Escape input
+  $id_safe = $linkId->real_escape_string($id);
+  $english_safe = $linkId->real_escape_string($english);
+  $translation_safe = $linkId->real_escape_string($translation);
+  
+  if (!$id_safe || !$english_safe || !$translation_safe ) {
+    $arr_response['success'] = 'incorrect';
+    send_data($arr_response);
+  } else {
+
+    $sql = "UPDATE fc_german_phrases"
+      . " SET english = '$english_safe', "
+      . " translation = '$translation_safe' "
+      . " WHERE id = $id_safe";
+ 
+    $result = $mySqli->handleQuery($sql);
+
+    if ($result) {
+      $arr_response['success'] = 'updated';
+    } else {
+      $arr_response['success'] = false;
+    }
+  }
+  
+  send_data($arr_response);
+  
+} elseif ($pos && $pos === 'verb') {
+  // Sanitize input
+  $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+  $english = filter_input(INPUT_POST, 'english', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $translation = filter_input(INPUT_POST, 'translation', FILTER_SANITIZE_STRING);
+  $separable = filter_input(INPUT_POST, 'separable', FILTER_SANITIZE_STRING);
+  $ich = filter_input(INPUT_POST, 'ich', FILTER_SANITIZE_STRING);
+  $du = filter_input(INPUT_POST, 'du', FILTER_SANITIZE_STRING);
+  $er_sie_es = filter_input(INPUT_POST, 'er_sie_es', FILTER_SANITIZE_STRING);
+  $wir = filter_input(INPUT_POST, 'wir', FILTER_SANITIZE_STRING);
+  $ihr = filter_input(INPUT_POST, 'ihr', FILTER_SANITIZE_STRING);
+  $sie_sie = filter_input(INPUT_POST, 'sie_sie', FILTER_SANITIZE_STRING);
+  
+  // Escape input
+  $id_safe = $linkId->real_escape_string($id);
+  $english_safe = $linkId->real_escape_string($english);
+  $translation_safe = $linkId->real_escape_string($translation);
+  $ich_safe = $linkId->real_escape_string($ich);
+  $du_safe = $linkId->real_escape_string($du);
+  $er_sie_es_safe = $linkId->real_escape_string($er_sie_es);
+  $wir_safe = $linkId->real_escape_string($wir);
+  $ihr_safe = $linkId->real_escape_string($ihr);
+  $sie_sie_safe = $linkId->real_escape_string($sie_sie);
+  
+  if (!$id_safe || !$english_safe || !$translation_safe || !$ich_safe || !$du_safe || !$er_sie_es_safe || !$wir_safe || !$ihr_safe || !$sie_sie_safe ) {
+    $arr_response['success'] = 'incorrect';
+    send_data($arr_response);
+  } else {
+    
+    $sql = "UPDATE fc_german_verbs"
+      . " SET english = '$english_safe', "
+      . " translation = '$translation_safe', "
+      . " separable = '$separable', "
+      . " ich = '$ich_safe', "
+      . " du = '$du_safe', "
+      . " er_sie_es = '$er_sie_es_safe', "
+      . " wir = '$wir_safe', "
+      . " ihr = '$ihr_safe', "
+      . " sie_Sie = '$sie_sie_safe' "
+      . " WHERE id = $id_safe";
+
+    $result = $mySqli->handleQuery($sql);
+
+    if ($result) {
+      $arr_response['success'] = 'updated';
+    } else {
+      $arr_response['success'] = false;
+    }
+  }
+ 
+  send_data($arr_response);
+  
+} elseif ($pos && $pos === 'category') {
+  // Sanitize input
+  $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  
+  // Escape input
+  $name_safe = $linkId->real_escape_string($name);
+  $id_safe = $linkId->real_escape_string($id);
+  
+  if (!$id_safe || !$name_safe) {
+    $arr_response['success'] = 'incorrect';
+  } else {
+    
+    $sql = "UPDATE fc_categories"
+      . " SET category = '$name_safe' "
+      . " WHERE id = $id_safe";
+
+    $result = $mySqli->handleQuery($sql);
+
+    if ($result) {
+      $arr_response['success'] = 'updated';
+    } else {
+      $arr_response['success'] = false;
+    }
+  }
+  
+  send_data($arr_response);
+  
+} else {
+  $arr_response['success'] = 'incorrect';
+  send_data($arr_response);
+}
