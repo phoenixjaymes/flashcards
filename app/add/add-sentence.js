b92c18804363298b37bf9255e2b4a0873eaf792a
Jaymes Young <jaymes@phoenixjaymes.com>
2017-02-12 18:29:11 -0800

    intial commit

---
+++ app/add/add-sentence.js
@@ -0,0 +1,44 @@
+/* 
+ File     : add-adjective.js
+ Date     : 05 Jan 2016
+ Author   : Jaymes Young <jaymes@phoenixjaymes.com>
+ */
+
+'use strict';
+
+angular.module('flashcards')
+  .controller('AddAdjective', function($scope, addItemService) {
+    
+    $scope.responseMessage;
+    $scope.showUpperCase = false;
+    
+    // Add word
+    $scope.addAdjective = function() {
+      $scope.displayFormMessage = false;
+      
+      addItemService.addItem($scope.formAddAdjective, function(response) {
+        // Check response message
+        if(response.data.success === true) {
+          // Clear form
+          $scope.formAddAdjective.english = '';
+          $scope.formAddAdjective.translation = '';
+          $scope.formAddAdjective.example = '';
+          $scope.formAddAdjective.img = '';
+          
+          $scope.displayMessage('true');
+        } else if (response.data.success === 'duplicate') {
+          $scope.displayMessage('duplicate');
+        } else if (response.data.success === 'incorrect') {
+          $scope.displayMessage('incorrect');
+        } else if (response.data.success === false) {
+          $scope.displayMessage('false');
+        }
+        
+        if(response.data.success === true) {
+          $scope.messageSuccess = true;
+        } else {
+          $scope.messageSuccess = false;
+        }
+      });
+    }; 
+});
---
+++ assets/inc/fc-get-sentences.php
@@ -0,0 +1,101 @@
+<?php
+/* 
+ * File Name: fc-get-update-words.php
+ * Date: 08 Jan 16
+ * Programmer: Jaymes Young-Liebgott
+ */
+
+require 'db-constants.php';
+require '../classes/database.php';
+require 'fc-utilities.php';
+
+// Set content ype to json
+header('content-type: application/json; charset=utf-8');
+
+// Databse connection
+$mySqli = new Database(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+$mySqli->getConnection();
+  
+$arr_words = [];
+$pos = filter_input(INPUT_GET, 'pos', FILTER_SANITIZE_STRING);
+
+if ($pos === 'adjective') {
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  
+  // Adjectives
+  $sql = "SELECT id, english, translation FROM fc_german_adjectives ORDER BY last_practiced LIMIT 10";
+  
+  
+  $result = $mySqli->handleQuery($sql);
+
+
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_word = [];
+    $arr_word['id'] = $row['id'];
+    $arr_word['word'] = $row['english'];
+    
+    $arr_word1 = [];
+    $arr_word1['id'] = $row['id'];
+    $arr_word1['word'] = $row['translation'];
+
+    $arr_words[] = $arr_word;
+    $arr_words[] = $arr_word1;
+  }
+  
+  shuffle($arr_words);
+  send_data($arr_words); 
+  
+} elseif ($pos === 'noun') {
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  
+  // Nouns
+  $sql = "SELECT id, english, translation FROM fc_german_nouns ORDER BY last_practiced LIMIT 10";
+  
+  $result = $mySqli->handleQuery($sql);
+
+
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_word = [];
+    $arr_word['id'] = $row['id'];
+    $arr_word['word'] = $row['english'];
+    
+    $arr_word1 = [];
+    $arr_word1['id'] = $row['id'];
+    $arr_word1['word'] = remove_parenthisis($row['translation']);
+
+    $arr_words[] = $arr_word;
+    $arr_words[] = $arr_word1;
+  }
+  
+  shuffle($arr_words);
+  send_data($arr_words); 
+  
+} elseif ($pos === 'verb') {
+  // Verbs
+  $category = filter_input(INPUT_GET, 'category', FILTER_SANITIZE_STRING);
+  $sort = filter_input(INPUT_GET, 'sort', FILTER_SANITIZE_STRING);
+  
+  
+  $sql = "SELECT id, english, infinitive FROM fc_german_verbs ORDER BY last_practiced LIMIT 10";
+  
+  $result = $mySqli->handleQuery($sql);
+  
+  // Get cards
+  while($row = $result->fetch_assoc()) {
+    $arr_word = [];
+    $arr_word['id'] = $row['id'];
+    $arr_word['word'] = $row['english'];
+    
+    $arr_word1 = [];
+    $arr_word1['id'] = $row['id'];
+    $arr_word1['word'] = $row['infinitive'];
+
+    $arr_words[] = $arr_word;
+    $arr_words[] = $arr_word1;
+  }
+  
+  shuffle($arr_words);
+  send_data($arr_words);
+}
