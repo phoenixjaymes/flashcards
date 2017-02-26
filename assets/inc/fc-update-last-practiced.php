2314fcd71b73f7e63988c6a61b599bf9a10ce2ff
Jaymes Young <jaymes@phoenixjaymes.com>
2017-02-25 20:51:33 -0800

    refactored code to use a single function

--- assets/inc/fc-update-last-practiced.php
+++ assets/inc/fc-update-last-practiced.php
@@ -14,135 +14,19 @@ header('content-type: application/json; charset=utf-8');
 
 // Databse connection
 $mySqli = new Database(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
-  $isConnected = $mySqli->getConnection();
+$isConnected = $mySqli->getConnection();
 
-// Set linkId if connected
- $isConnected ? $linkId = $mySqli->getLink() : $linkId = false;
- 
- /*
-  * mysqli->real escape string should be added to class
-  */
- 
- 
 $arr_response = [];
-$date = date('Y-m-d');
+date_default_timezone_set('America/Los_Angeles');
+$date = date('Y-m-d H:i:s');
 $pos = filter_input(INPUT_POST, 'pos', FILTER_SANITIZE_STRING);
-
-//print_r($_POST);
-//exit();
+$ids = filter_input(INPUT_POST, 'ids', FILTER_SANITIZE_STRING);
 
 
-//
-if ($pos && $pos === 'adjective') {
-  // Sanitize input
-  $ids = filter_input(INPUT_POST, 'ids', FILTER_SANITIZE_STRING);
-  
-  // Escape input
-  $ids_safe = $linkId->real_escape_string($ids);
-   
-  // Check of empty values
-  if (!$ids_safe) {
-    $arr_response['success'] = 'incorrect';
-  } else {
-         
-    $sql = "UPDATE fc_german_adjectives"
-      . " SET last_practiced = '$date' "
-      . " WHERE id IN ($ids_safe)";
-
-    $result = $mySqli->handleQuery($sql);
-
-    if ($result) {
-      $arr_response['success'] = 'updated';
-    } else {
-      $arr_response['success'] = false;
-    }
-  }
+if ( $pos && $ids ) {
+  $arr_response = update_last_practiced($mySqli, $pos, $ids);
   
   send_data($arr_response);
-  
-} elseif ($pos && $pos === 'noun') {
-  // Sanitize input
-  $ids = filter_input(INPUT_POST, 'ids', FILTER_SANITIZE_STRING);
-  
-  // Escape input
-  $ids_safe = $linkId->real_escape_string($ids);
-  
-  if (!$ids_safe) {
-    $arr_response['success'] = 'incorrect';
-    send_data($arr_response);
-  } else {
-
-    $sql = "UPDATE fc_german_nouns"
-      . " SET last_practiced = '$date' "
-      . " WHERE id IN ($ids_safe)";
-
-    $result = $mySqli->handleQuery($sql);
-
-    if ($result) {
-      $arr_response['success'] = 'updated';
-      
-    } else {
-      $arr_response['success'] = false;
-    }
-  }
-  
-  send_data($arr_response);
-    
-} elseif ($pos && $pos === 'phrase') {
-  // Sainitize input
-  $ids = filter_input(INPUT_POST, 'ids', FILTER_SANITIZE_STRING);
-  
-  // Escape input
-  $ids_safe = $linkId->real_escape_string($ids);
-  
-  if (!$ids_safe ) {
-    $arr_response['success'] = 'incorrect';
-    send_data($arr_response);
-  } else {
-
-    $sql = "UPDATE fc_german_phrases"
-      . " SET last_practiced = '$date' "
-      . " WHERE id IN ($ids_safe)";
- 
-    $result = $mySqli->handleQuery($sql);
-
-    if ($result) {
-      $arr_response['success'] = 'updated';
-    } else {
-      $arr_response['success'] = false;
-    }
-  }
-  
-  send_data($arr_response);
-  
-} elseif ($pos && $pos === 'verb') {
-  // Sanitize input
-  $ids = filter_input(INPUT_POST, 'ids', FILTER_SANITIZE_STRING);
-  
-  // Escape input
-  $ids_safe = $linkId->real_escape_string($ids);
-  
-  
-  if (!$ids_safe ) {
-    $arr_response['success'] = 'incorrect';
-    send_data($arr_response);
-  } else {
-    
-    $sql = "UPDATE fc_german_verbs"
-      . " SET last_practiced = '$date' "
-      . " WHERE id IN ($ids_safe)";
-
-    $result = $mySqli->handleQuery($sql);
-
-    if ($result) {
-      $arr_response['success'] = 'updated';
-    } else {
-      $arr_response['success'] = false;
-    }
-  }
- 
-  send_data($arr_response);
-  
 } else {
   $arr_response['success'] = 'incorrect';
   send_data($arr_response);
